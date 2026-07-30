f8d5f8a5546433f58342ec4205440fc96460d8cc
Deepak <dm06124081@gmail.com>
2026-07-30 19:41:59 +0530

    first commit - starting C practice

---
+++ .vscode/Untitled-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <string.h>
+
+const char* determine_color(const char* color) { 
+    // Write your logic here to determine the color based on the sting s.
+    // Return "Black" or "White" based on the problem statement.
+     return NULL; 
+     }
+
+     int main(){ 
+      char s[256];
+      scanf("%s" , s);
+       const char* result = determine_color(s);
+       printf("%s\n", result);
+     return 0;
+    }
---
+++ 01_practice.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int length = 3;
+    int breadth = 6;
+    printf("Area of this rectangle is: %d\n", length * breadth);
+    return 0;
+}
