a324bd7a2250abeb7ece3a2b4b3fb5d908e91fa2
Deepak <dm06124081@gmail.com>
2026-08-01 19:24:01 +0530

    added rectangle area program - hardcoded and user input versions

---
+++ 02_practice.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int length = 5 ;
+    int breadth = 8 ;
+    
+    int area = length * breadth ;
+    printf("area of rectangle is %d\n", area);
+        return 0;
+}
