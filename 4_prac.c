a6a9da9beaac5aaa65bcaa94a3a399983f4f6853
Deepak <dm06124081@gmail.com>
2026-08-01 22:34:27 +0530

    added 4_prac.c - variable  declarations

---
+++ 4_prac.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Declare an integer variable 'a'
+    int a = 2;
+    
+    // Declare a float variable 'b'
+    float b = 3.5;
+    
+    // Declare and initialize multiple variables
+    int c = 4, d = 5;
+    
+    printf("a = %d\n", a);
+    printf("b = %.2f\n", b);
+    printf("c = %d\n", c);
+    printf("d = %d\n", d);
+    
+    return 0;
+}
