a8e633fc03551706d4ed8ab517783033684c2618
Deepak <dm06124081@gmail.com>
2026-08-01 22:18:01 +0530

    added 3_prac.c

---
+++ .vscode/3_prac.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 10;
+    printf("this is %d\n", i);
+    // %d for integers, %f for floating point values, %c for characters
+    
+    return 0;
+}
