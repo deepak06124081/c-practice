9f775fafa53778536ae704437b616a3f22206ce5
Deepak <dm06124081@gmail.com>
2026-08-02 11:39:37 +0530

    added 8prac.c

---
+++ 8prac.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n=45;
+    float m = 32.23;
+
+    n = (int) m; // convert the data type to int 
+    printf("The value is %d\n", n);
+
+    return 0;
+}
