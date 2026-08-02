fa3dd34f6e39a1a58c49e99b23e4a543953366e2
Deepak <dm06124081@gmail.com>
2026-08-02 16:14:28 +0530

    added 1conditionals.c

---
+++ .vscode/CHAPTER 3 prac/1conditionals.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 15;
+
+    if(age>10){
+       printf("We are inside if\n");
+     printf("Your age is greater than 10");
+    }
+    return 0;
+}
