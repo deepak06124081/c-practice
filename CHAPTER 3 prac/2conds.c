f26d9e4bbc34c33019c43e7dfe0c6669f884695c
Deepak <dm06124081@gmail.com>
2026-08-02 16:25:27 +0530

    added 2conds.c

---
+++ CHAPTER 3 prac/2conds.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+ 
+ int main(void)
+ {
+    if(1){
+        printf("This if is executed!\n");
+    }
+    if(2345){
+        printf("This if is also executed!\n");
+    }
+    return 0;
+ }
