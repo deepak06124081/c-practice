251b4390fbed83b41799c030b0a9e029f4c0289b
Deepak <dm06124081@gmail.com>
2026-08-02 18:48:25 +0530

    added conditional and logical practice files

---
+++ C_Programs/CHAPTER 3 prac/4else_if.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 65 ;
+
+    if(age>60){
+        printf("You can drive and you are a senoir citizen");
+      }
+    else if (age>18){
+    printf("Yiu can drive and you are an adult");
+
+}   
+else{
+    printf("You can't drive");
+} 
+
+       return 0;
+}
