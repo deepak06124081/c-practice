b4dc6b3a82368ab60dec8c308837c2fda268f1b5
Deepak <dm06124081@gmail.com>
2026-08-02 11:12:01 +0530

    added 5_prac.c

---
+++ 5_prac.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+ float a = 9.0;
+ int b= 2;
+ float c = a/b;
+ int d = 6.7;
+ printf("The value of c is: %f\n", c);
+ printf("The value of d is %d\n", d);    
+    return 0;
+}
