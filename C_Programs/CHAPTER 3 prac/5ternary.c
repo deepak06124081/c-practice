4cf873064ee3c118e79b8f0f490a7b666a737dd3
Deepak <dm06124081@gmail.com>
2026-08-03 09:22:02 +0530

    added 5ternary.c

---
+++ C_Programs/CHAPTER 3 prac/5ternary.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // conditons ? expression-if -true : expression-if-false
+    int a = 345;
+    int b= 32;
+    a>b ? printf("a is greater") : printf("b is greater");
+    return 0;
+}
