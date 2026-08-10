fbc1812bf655f702580fb36f85ccdee0046d7c48
Deepak <dm06124081@gmail.com>
2026-08-10 09:14:49 +0530

    added 12_prac.c

---
+++ C_Programs/CHAPTER 5 prac/11_prac.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+ 
+ int sum_natural(int);
+
+ int sum_natural(int n){
+     if(n==1){
+        return 1 ;
+
+     }
+     // sum(n) = 1 + 2 + 3 + ....... n -1 + n;
+     // sum(n) = sum(n-1) + n;
+     return sum_natural(n-1) + n;
+ }
+ 
+ int main(void)
+ {
+    printf("The sum of first 5 natural no's is %d", sum_natural(5));    
+    return 0;
+ }
---
+++ C_Programs/CHAPTER 5 prac/8_prac.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+float force(float);
+
+float force(float mass){
+    return mass*9.8;
+} 
+int main(void)
+{
+    int m = 45;
+    printf("The value of force is %f\n", force(m));
+    
+    return 0;
+}
