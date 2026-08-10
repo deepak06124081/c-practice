73085171a3faacd1ed5c5a7653283a9a39ac7a9a
Deepak <dm06124081@gmail.com>
2026-08-10 10:48:52 +0530

    added 1_pointer.c

--- C_Programs/CHAPTER 6 prac/1_pointer.c
+++ C_Programs/CHAPTER 6 prac/1_pointer.c
@@ -5,7 +5,7 @@ int main(void)
     int i = 72 ;
     int*j = &i; // j is pointer poinying to i 
     printf("The address of i %p\n", &i);
-    printf("The address of j %p\n", j);
+    printf("The address of i %p\n", j);
 
     return 0;
 } 
