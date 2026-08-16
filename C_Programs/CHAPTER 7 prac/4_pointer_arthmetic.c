2410c7105499bcef38e0a445dbde5f482df60b03
Deepak <dm06124081@gmail.com>
2026-08-16 10:27:31 +0530

    added 1_array.c, 2_array_input.c, 3_other_array.c, 4_pointer_arthmetic.c, 5_array_in_memory.c

---
+++ C_Programs/CHAPTER 7 prac/4_pointer_arthmetic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 5;
+    int *ptr = &a;
+    printf("The address of a is %u\n", &a);
+    printf("The address of a is %u\n", ptr);
+    ptr++;
+    printf("The value of ptr is %u\n", ptr);
+
+    return 0;
+}
